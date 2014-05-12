31e21a4e655bcccebec485326f8d53bed7d8dd09
Matthew Hasteley <matthasteley@gmail.com>
2014-05-12 20:26:51 +0100

    Sorted out the resize index slides bug

--- js/index.js
+++ js/index.js
@@ -2,11 +2,6 @@ document.getElementById('banner-pic').style.margin = '0 0 -263px 0';
 
 $('#slideshow').cycle({timeout: 3000});
 
-
-$(window).resize(function() {
-    if ($(window).width() < 333) {
-        location.reload();
-    }
-    else {
-    }
-});
+window.onorientationchange = function() { 
+    window.location.reload();
+};
