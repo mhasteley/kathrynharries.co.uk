3483061b996af6fefe4b6f118582c70c870defa5
Matthew Hasteley <matthasteley@gmail.com>
2014-05-15 15:02:36 +0100

    Added background toggle so lazy load looks better

--- js/lazy.js
+++ js/lazy.js
@@ -18,12 +18,41 @@ $(function() {
                 myImg.style.height = "auto";
                 myImg.style.margin = "0 auto 15px auto";
                 $('.gallHD').attr('href', writeGalleryHD);
-                $('.md-content').css('opacity', '1');
                 $('.md-content').css('background-color', 'rgba(0, 0, 0, 0.4)');
             };
         };
     }
     ;
 });
+
+$('button.md-close').click(function(){
+    $('.md-content').css('background-color', 'rgba(0, 0, 0, 0)');
+});
+
+
+/*
+
+$('.square').toggle(function () {
+    $(".md-content").addClass("overlay-active");
+}, function () {
+    $(".md-content").removeClass("overlay-active");
+});
+
+alt
+
+$('#user_button').click(function() {
+    $('#user_options').toggle();
+    $(this).toggleClass('active');
+    return false;
+})
+
+$('button.square').click(function() {
+    $('div.md-content').toggle();
+    $(this).addClass('overlay-active');
+    return false;
+});
+
+
+*/
 
 // md-content - remove background while lazy-loading / space HD link - probably need to toggle this...
